0e79ac0c8fe6bcdc0baa6de82ed2a8dad2c34fde
elara-mendes <elaramendes@yahoo.com>
2025-01-26 20:38:44 -0300

    Created functions
    Created functions to add participants, to draw and to reset

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 let participants = [];
-let participant = "";
-let item = prompt("Escreva um item:");
+let participant = document.getElementById("participant");
+let item = document.getElementById("item");
+let participantsList = document.getElementById("participantsList");
+let winnerResult = document.getElementById("winnerResult");
+let titleItem = document.getElementById("title");
 
-if (item != "") {
-    while (participant != "parar") {
-        participant = prompt("Escreva um nome:")
-        if (participant != "parar" && participant != "") {
-            participants.push(participant)
-        }
+function Add() {
+    if (item.value !== "") {
+        titleItem.innerHTML = item.value;
+        item.value = "";
     }
 
+    if (participant.value !== "") {
+        participants.push(participant.value);
+        participantsList.innerHTML += `<p>${participant.value}</p>`;
+        participant.value = "";
+    }
+}
+
+function Draw() {
     if (participants.length > 0) {
         for (let i = participants.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [participants[i], participants[j]] = [participants[j], participants[i]]; // Fisher-Yates (ou Knuth shuffle)
         }
-        console.log(`${participants[0]} é o vencedor e ganhou ${item}`)
     } else {
-        console.log("Nenhum participante adicionado.")
+        console.log("Anyone was added.")
     }
+    if (titleItem.innerHTML !== "") {
+        winnerResult.innerHTML += `${participants[0]} its the winner! and got ${titleItem.innerHTML}!`;
+        participantsList.innerHTML = "";
+    }
+}
+
+function Reset() {
+    winnerResult.innerHTML = "";
+    titleItem.innerHTML = "";
+    participantsList.innerHTML = "";
+    item.value = "";
+    participant.value = "";
 }
