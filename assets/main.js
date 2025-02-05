6403393108433cbc419b22e1c32ed44cfc5f5b50
elara-mendes <elaramendes@yahoo.com>
2025-02-05 20:32:50 -0300

    Improving logic

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,11 @@
-let participants = [];
+let participants = [] ;
 let winners = [];
 let participant = document.getElementById("participant");
 let item = document.getElementById("item");
 let participantsList = document.getElementById("participantsList");
 let winnerResult = document.getElementById("winnerResult");
 let titleItem = document.getElementById("title");
+let drawButton = document.getElementById("drawButton");
 
 function Add() {
     if (item.value !== "") {
@@ -21,27 +22,33 @@ function Add() {
 }
 
 function Draw() {
-    if (participants.length > 0) {
-        for (let i = participants.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [participants[i], participants[j]] = [participants[j], participants[i]]; // Fisher-Yates (ou Knuth shuffle)
+        if (participants.length > 0) {
+            for (let i = participants.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [participants[i], participants[j]] = [participants[j], participants[i]]; // Fisher-Yates (ou Knuth shuffle)
+            }
+        } else {
+            console.log("Anyone was added.");
         }
-    } else {
-        console.log("Anyone was added.");
-    }
-    if (titleItem.innerHTML !== "") {
-        winnerResult.innerHTML += `${participants[0]} its the winner! and got ${titleItem.innerHTML}!`;
-        participants.shift(participant[0]);
-        participantsList.innerHTML = participants.map(p => `<p>${p}</p>`).join("");
-        // participantsList.innerHTML = "";
-        // titleItem.innerHTML = "";
-        // item.style.display = "inline"
-        // participants.length = 0
-    }
 
-    setTimeout(() => {
-        winnerResult.innerHTML = "";
-    }, 2000);
+        if (titleItem.innerHTML !== "") {
+            winnerResult.innerHTML += `${participants[0]} its the winner! and got ${titleItem.innerHTML}!`;
+            participants.shift(participant[0]);
+            console.log(participants)
+            participantsList.innerHTML = participants.map(p => `<p>${p}</p>`).join("");
+        }
+
+        if (participants.length == 0) {
+            titleItem.innerHTML = "";
+            item.style.display = "inline";
+        }
+
+        drawButton.setAttribute("disabled", "true");
+        
+        setTimeout(() => {
+            winnerResult.innerHTML = "";
+            drawButton.removeAttribute("disabled");
+        }, 1500);
 }
 
 function Reset() {
